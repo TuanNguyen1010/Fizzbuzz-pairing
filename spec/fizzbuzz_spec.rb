bd9413473131000b9cc7b6d4df2bf0d75d481b72
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-08 13:33:16 +0000

    Pair coding completed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,16 @@ describe "fizzbuzz" do
   it 'returns "fizz" when passes 3' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+
+  it 'returns "buzz" when passes 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it 'returns "fizzbuzz" when passes 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'returns number if not pass 3 & 5' do
+    expect(fizzbuzz(8)).to eq "8"
+  end
 end
